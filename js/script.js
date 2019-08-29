6b85dc1b8270251839206f2dcfe959169629b077
juljaVysotska <julja.vysotska@gmail.com>
2019-08-29 18:30:08 +0300

    -change file structure
    -fix photos
    -add scrollify
    -add fonts

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// (function preloader(){
+// 	var pr = document.getElementById('firstPage');
+// 	if(pr) 
+// 	{	
+// 		setTimeout(function(){
+// 			document.getElementById('firstPage').classList.add('first-page');
+// 			setTimeout(" document.getElementById('firstPage').classList.remove('preloader');",3500);	
+// 		},3000);
+
+// 	}	
+
+
+// })()
+

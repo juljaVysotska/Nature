926dcff2632a0dacb0cba51f310aa7dc6b6adec0
juljaVysotska <julja.vysotska@gmail.com>
2019-08-30 12:35:58 +0300

    compress images

--- js/script.js
+++ js/script.js
@@ -13,3 +13,29 @@
 
 // })()
 
+ window.addEventListener('scroll', function () {
+ 	//console.log('true');
+
+
+ 	let scrolled = window.pageYOffset;
+
+
+ 	let elems = document.querySelectorAll('p.letter');
+ 	//elems.setAttribute('transform', 'translateY('+ scrolled * -.2+'px)');
+
+ 	for(var i =0; i< elems.length; i++){
+
+ 		elems[i].setAttribute('style', 'transform: translateY('+ scrolled * -.09+'px)');
+ 	}
+
+
+ 	let sectionElems = document.querySelectorAll('div.section');
+
+ 	for(let i = 0 ;i < sectionElems.length; i++){
+ 			if(sectionElems[i].pageYOffset - window.pageYOffset == 0){
+ 				scrolled = scrolled = sectionElems[i].pageYOffset;
+ 			}
+ 	}
+
+
+ });
